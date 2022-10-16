b80183838d3ace9d5239a789e389851e2009ac39
Rehan Virani <rehanvirani29@gmail.com>
2022-10-15 21:59:31 -0400

    Started own version of code

---
+++ Accounts/Account/Account/Main Access.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+
+
+
+int main()
+
+{
+	int option;
+
+
+	cout << "Welcome to the record sysem. There are may differnt ";
+	cout << "actions that can be done. Please select one of the following" << endl;
+	cout << "5: Exit System" << endl;
+
+	switch (option) {
+	case 1:
+
+
+	}
+
+
+
+
+}
