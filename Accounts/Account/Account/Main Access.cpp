308c8d638732cc436b39fc90f3509feec856205c
Rehan Virani <rehanvirani29@gmail.com>
2022-10-18 21:45:22 -0400

    improved new account and added loop to main switch. Next need to fix the issue with var for record number for ability to reuse appliucation

--- Accounts/Account/Account/Main Access.cpp
+++ Accounts/Account/Account/Main Access.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <filesystem>
 using std::string;
+using std::to_string;
 using std::cout;
 using std::cin;
 using std::ios;
 using std::endl;
 using std::ofstream;
+using std::ifstream;
 
 
 class account_options {
@@ -15,25 +17,35 @@ private:
 	
 
 public:
-	int record_number=1;
+	int record_number;
 
 	void new_account();
 	void view_accounts();
+	void exit();
 
 };
 
 void account_options::new_account()
 {
+	ifstream check_file;
+	check_file.open("Master_Record.txt");
+	if(!check_file){
+		record_number = 0;
+	}
+	
+
 
 	string filename; // name of record to create
 	string filename_complete; // name of complete file name with .txt to add to master record
+	string record_number_string = to_string(record_number);
 	cout << "Enter name of new file to create no spaces please!" << endl;
 	cin >> filename;
-	filename_complete = filename + ".txt"; //Complete file name is temp generated to be added to master record
+	filename_complete = record_number_string+  ": " + filename + ".txt"; //Complete file name is temp generated to be added to master record
 	
 	ofstream outfile;
 	outfile.open(filename + ".txt", ios::binary | ios::app);
 	/*
+	
 	Here is code for information to be added to file
 	
 	*/
@@ -43,9 +55,17 @@ void account_options::new_account()
 	outfile << filename_complete << endl;
 	outfile.close();
 
+	record_number++ ;
 }
 
 void account_options::view_accounts()
+{
+
+
+}
+
+
+void account_options::exit()
 {
 
 
@@ -57,35 +77,39 @@ int main()
 	int option;
 	account_options A;
 
-	cout << "Welcome to the record sysem. There are may differnt ";
-	cout << "actions that can be done. Please select one of the following" << endl;
-	cout << "1: Create new account" << endl;
-	cout << "2: View Accounts" << endl;
-	cout << "5: Exit System" << endl;
-	cin >> option;
+	while (bool loop = true) {
 
-	switch (option) {
-	case 1:
-		A.new_account();
-		break;
-	case 2:
-		A.view_accounts();
-		break;
-	case 3:
+		cout << "Welcome to the record sysem. There are may differnt ";
+		cout << "actions that can be done. Please select one of the following" << endl;
+		cout << "1: Create new account" << endl;
+		cout << "2: View Accounts" << endl;
+		cout << "5: Exit System" << endl;
+		cin >> option;
 
-		break;
-	case 4:
+		switch (option) {
+		case 1:
+			A.new_account();
+			break;
+		case 2:
+			A.view_accounts();
+			break;
+		case 3:
 
-		break;
-	case 5:
-		exit(0);
-		break;
-	default:
-		cout << "\nEnter corret choice";
-		exit(0);
+			break;
+		case 4:
+
+			break;
+		case 5:
+			A.exit();
+			exit(0);
+			loop = false;
+			break;
+		default:
+			cout << "\nEnter corret choice";
+
+		}
+
 	}
-
-
 
 
 }
